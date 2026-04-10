20584681f44ab3060925fba5e4862b6f2179c06f
kev-dev-v <hh.andress78@gmail.com>
2026-04-10 05:35:26 -0500

    feat(perfiles): Uploading the completed module fully integrated with the develop

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'observatorio_cti';
 $username = 'root';
-$password = 'root';
+$password = '';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
